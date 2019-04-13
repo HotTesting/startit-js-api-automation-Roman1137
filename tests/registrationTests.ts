694403f76e93d8b49890e7467d74bc48ca6c412a
Roman1137 <poma.borodavka@gmail.com>
2019-04-13 17:05:57 +0300

    removed redundunt test

--- tests/registrationTests.ts
+++ tests/registrationTests.ts
@@ -19,31 +19,20 @@ describe('Registration tests', async () => {
 
     describe(' negative cases', async () => {
 
-        it('should not register the same user twice', async () => {
-            let user = User.CreateValid();
-
-            await registrationService.registerValid(user);
-
-            let response = await registrationService.registerInValid(user);
-
-            expect(response.body.error).to.eql(403);
-            expect(response.body.reason, "Username already exists")
-        });
-
         it('should not register user with the same email twice', async () => {
             // arrange
             let user = User.CreateValid();
 
             await registrationService.registerValid(user);
 
-            let userSecond = User.Create()
+            let userNew = User.Create()
                 .withEmail(user.email)
                 .withPassword()
                 .withUserName()
                 .build();
 
             // act
-            let response = await registrationService.registerInValid(userSecond);
+            let response = await registrationService.registerInValid(userNew);
 
             expect(response.body.error).to.eql(403);
             expect(response.body.reason, "Email already exists")
@@ -55,14 +44,14 @@ describe('Registration tests', async () => {
 
             await registrationService.registerValid(user);
 
-            let userSecond = User.Create()
+            let usernNew = User.Create()
                 .withEmail()
                 .withPassword()
                 .withUserName(user.username)
                 .build();
 
             // act
-            let response = await registrationService.registerInValid(userSecond);
+            let response = await registrationService.registerInValid(usernNew);
 
             expect(response.body.error).to.eql(403);
             expect(response.body.reason, "Username already exists")
